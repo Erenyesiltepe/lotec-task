6a4b327766d89643bf3a075c0fec8e5c46aeb94e
Eren Yeşiltepe <eren.bilkentk15@hotmail.com>
2025-12-17 17:11:45 +0300

    bugfix: backend cors library

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import taskRoutes from './routes/taskRoutes';
 import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
